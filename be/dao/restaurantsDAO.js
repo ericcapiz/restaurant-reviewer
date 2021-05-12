1bf1a7e132580f5b0d544d1a6a74b0972413eb0d
ericcapiz <ericcapiz@gmail.com>
2021-05-12 14:48:24 -0500

    backend works and complete

--- be/dao/restaurantsDAO.js
+++ be/dao/restaurantsDAO.js
@@ -1,3 +1,6 @@
+import mongodb from "mongodb"
+const ObjectId = mongodb.ObjectID
+
 let restaurants
 
 //connect to db and get data from table
@@ -61,6 +64,62 @@ export default class RetaurantsDAO {
             `Unable to convert cursor to array of problem counting documents, ${e}`
         )
         return {restaurantsList: [], totalNumRestaurants: 0}
+        }
+    }
+
+    static async getRestaurantByID(id) {
+        try {
+          const pipeline = [
+            {
+                $match: {
+                    _id: new ObjectId(id),
+                },
+            },
+                  {
+                      $lookup: {
+                          from: "reviews",
+                          let: {
+                              id: "$_id",
+                          },
+                          pipeline: [
+                              {
+                                  $match: {
+                                      $expr: {
+                                          $eq: ["$restaurant_id", "$$id"],
+                                      },
+                                  },
+                              },
+                              {
+                                  $sort: {
+                                      date: -1,
+                                  },
+                              },
+                          ],
+                          as: "reviews",
+                      },
+                  },
+                  {
+                      $addFields: {
+                          reviews: "$reviews",
+                      },
+                  },
+              ]
+          return await restaurants.aggregate(pipeline).next()
+        } catch (e) {
+          console.error(`Something went wrong in getRestaurantByID: ${e}`)
+          throw e
+        }
+      }
+
+    static async getCuisines() {
+        let cuisines = []
+        try {
+            //distinct just returns one of each cuisine
+            cuisines = await restaurants.distinct("cuisine")
+            return cuisines
+        } catch (e) {
+            console.log(`Unable to get cuisines : ${e}`)
+            return cuisines
         }
     }
 }
