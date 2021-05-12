d1a7fc93158cee365bf3710ae03e099e746c757a
ericcapiz <ericcapiz@gmail.com>
2021-05-12 11:22:46 -0500

    restaurant controller done

--- be/api/restaurants.controller.js
+++ be/api/restaurants.controller.js
@@ -1,12 +1,15 @@
-import e from 'express'
-import { removeAllListeners } from 'nodemon'
 import RestaurantsDAO from '../dao/restaurantsDAO.js'
 
 export default class RestaurantsController {
     static async apiGetRestaurants(req,res,next){
+        //query strings in url
+        //if restaurantsPerPage exists in the url as query, convert to interger and set it to 10
+        // if theres not a query in url, default is 20 per page 
         const restaurantsPerPage = req.query.restaurantsPerPage ? parseInt(req.query.restaurantsPerPage, 10) : 20
+        //if page is in query set it to 10 pages, if not then 0
         const page = req.query.page ? parseInt(req.query.page, 10) : 0
 
+        //will filter based on what shows in the url query
         let filters = {}
         if (req.query.cuisine){
             filters.cuisine = req.query.cuisine
@@ -15,7 +18,22 @@ export default class RestaurantsController {
         } else if (req.query.name) {
             filters.name = req.query.name
         }
+
+        const { restaurantsList, totalNumRestaurants} = await RestaurantsDAO.getRestaurants({
+            filters,
+            page,
+            restaurantsPerPage,
+        })
+
+        let response = {
+            restaurants: restaurantsList,
+            page:page,
+            filters:filters,
+            entries_per_page: restaurantsPerPage,
+            total_results: totalNumRestaurants,
+        }
+
+        res.json(response)
     }
 }
 
-//36 min mark
