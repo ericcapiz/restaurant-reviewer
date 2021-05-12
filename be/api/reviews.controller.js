54b900040c4481195c43eff8d19f04eadda1d256
ericcapiz <ericcapiz@gmail.com>
2021-05-12 13:30:44 -0500

    crud works for reviews

--- be/api/reviews.controller.js
+++ be/api/reviews.controller.js
@@ -1,72 +1,74 @@
-import ReviewsDAO from '../dao/reviewsDAO.js';
+import ReviewsDAO from "../dao/reviewsDAO.js"
 
 export default class ReviewsController {
     //CRUD methods for reviews
-    static async apiPostReview(req,res,next){
+  static async apiPostReview(req, res, next) {
+       //getting the info of the restaurant thats being reviewed
+    try {
+      const restaurantId = req.body.restaurant_id
+      const review = req.body.text
+      const userInfo = {
+        name: req.body.name,
+        _id: req.body.user_id
+      }
+      const date = new Date()
 
-        //getting the info of the restaurant thats being reviewed
-        try {
-            const restaurantId = req.body.restaurant_id
-            const review = req.body.text
-            const userInfo = {
-                name: req.body.name,
-                _id: req.body.user_id
-            }
-            const date = new Date()
-            
-            const ReviewResponse = await ReviewsDAO.addReview(
-                restaurantId,
-                userInfo,
-                review,
-                date,
-            )
-            res.json({status: "success"})
-        } catch (e) {
-            res.status(500).json({error: e.message})
-        }
+      const ReviewResponse = await ReviewsDAO.addReview(
+        restaurantId,
+        userInfo,
+        review,
+        date,
+      )
+      res.json({ status: "success" })
+    } catch (e) {
+      res.status(500).json({ error: e.message })
     }
+  }
 
-    static async apiUpdateReview(req,res,next){
-        //getting info on the review to update
-        try {
-            const reviewId = req.body.review_id
-            const text = req.body.text
-            const data = new Date()
-            
-            const ReviewResponse = await ReviewsDAO.updateReview(
-                reviewId,
-                req.body.user_id,
-                text,
-                date,
-            )
+  static async apiUpdateReview(req, res, next) {
+      //getting info on the review to update
+    try {
+      const reviewId = req.body.review_id
+      const text = req.body.text
+      const date = new Date()
 
-            var {error} = ReviewResponse
-            if (error) {
-                res.status(400).json({error})
-            }
+      const reviewResponse = await ReviewsDAO.updateReview(
+        reviewId,
+        req.body.user_id,
+        text,
+        date,
+      )
 
-            if (review.ReviewResponse.modifiedCount === 0) {
-                throw new Error(
-                    "Unable to update review - user may not be OP"
-                )
-            }
-        } catch (e) {
-            res.status(500).json({error: e.message})
-        }
+      var { error } = reviewResponse
+      if (error) {
+        res.status(400).json({ error })
+      }
+
+      if (reviewResponse.modifiedCount === 0) {
+        throw new Error(
+          "unable to update review - user may not be original poster",
+        )
+      }
+
+      res.json({ status: "success" })
+    } catch (e) {
+      res.status(500).json({ error: e.message })
     }
+  }
 
-    static async apiDeleteReview(req,res,next){
-        try {
-            const reviewId = req.query._id
-            const userId = req.body.user_id
-            console.log(reviewId)
-            const reviewResponse = await ReviewsDAO.deleteReview(
-                reviewId,
-                userId,
-            )
-            res.json({status: "success"})
-        } catch (e) {
-            res.status(500).json({error: e.message})
-        }
+  static async apiDeleteReview(req, res, next) {
+    try {
+      const reviewId = req.query.id
+      const userId = req.body.user_id
+      console.log(reviewId)
+      const reviewResponse = await ReviewsDAO.deleteReview(
+        reviewId,
+        userId,
+      )
+      res.json({ status: "success" })
+    } catch (e) {
+      res.status(500).json({ error: e.message })
     }
+  }
+
 }
--- be/dao/reviewsDAO.js
+++ be/dao/reviewsDAO.js
@@ -1,64 +1,64 @@
-import { text } from 'express'
-import mongo from 'mongodb'
-const ObjectId = mongodb.ObjectId
+import mongodb from "mongodb"
+const ObjectId = mongodb.ObjectID
 
 let reviews
+
 export default class ReviewsDAO {
-    static async injectDB(conn) {
-        if (reviews) {
-            return
-        }
-        try {
-            reviews = await conn.db(process.env.RESTREVIEWS_NS).collection("reviews")
-        } catch (e) {
-            console.error(`Unable to establish collection handles in userDAO: ${e}`)
-        }
+  static async injectDB(conn) {
+    if (reviews) {
+      return
     }
-
-    static async addReview(restaurantId, user, review, date){
-        try {
-            const reviewDoc = {
-                name: user.name, 
-                user_id: user._id,
-                date:date,
-                text:review,
-                restaurant_Id: ObjectId(restaurantId),
-            }
-
-            return await reviews.insertOne(reviewDoc)
-        } catch (e) {
-            console.error(`Unable to post review: ${e}`)
-            return {error: e}
-        }
+    try {
+      reviews = await conn.db(process.env.RESTREVIEWS_NS).collection("reviews")
+    } catch (e) {
+      console.error(`Unable to establish collection handles in userDAO: ${e}`)
     }
+  }
 
-    static async updateReview(userId, reviewId, date){
-        try {
-            const updateResponse = await reviews.updateOne(
-                //matching the review with the review id to make sure correct review is being updated
-                {user_id: userId, _id:ObjectId(reviewId)},
-                //posting the new updated with the new text/date
-                { $set: {text: text, date: date}},
-                )
+  static async addReview(restaurantId, user, review, date) {
+    try {
+      const reviewDoc = { name: user.name,
+          user_id: user._id,
+          date: date,
+          text: review,
+          restaurant_id: ObjectId(restaurantId), }
 
-                return updateResponse
-        } catch (e) {
-            console.error(`Unable to update review: ${e}`)
-            return {error: e}
-        }
+      return await reviews.insertOne(reviewDoc)
+    } catch (e) {
+      console.error(`Unable to post review: ${e}`)
+      return { error: e }
     }
+  }
 
-    static async deleteReview( userId, reviewId){
-        try {
-            const deleteResponse = await reviews.deleteOne({
-                _id: ObjectId(reviewId),
-                user_id: userId,
-            })
+  static async updateReview(reviewId, userId, text, date) {
+    try {
+      const updateResponse = await reviews.updateOne(
+          //makes sure the user and review id's match to op and the correct review
+        { user_id: userId, _id: ObjectId(reviewId)},
+        //posting the new updated with the new text/date
+        { $set: { text: text, date: date  } },
+      )
 
-            return deleteResponse
-        } catch (e) {
-            console.error(`Unable to delete review: ${e}`)
-            return {error: e}
-        }
+      return updateResponse
+    } catch (e) {
+      console.error(`Unable to update review: ${e}`)
+      return { error: e }
     }
+  }
+
+  static async deleteReview(reviewId, userId) {
+
+    try {
+      const deleteResponse = await reviews.deleteOne({
+        _id: ObjectId(reviewId),
+        user_id: userId,
+      })
+
+      return deleteResponse
+    } catch (e) {
+      console.error(`Unable to delete review: ${e}`)
+      return { error: e }
+    }
+  }
+
 }
