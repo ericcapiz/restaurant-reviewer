65e1e228d83dab4dc9f8b24838d8bb8673c542d4
ericcapiz <ericcapiz@gmail.com>
2021-05-12 12:09:34 -0500

    review controller done

--- be/api/restaurants.controller.js
+++ be/api/restaurants.controller.js
@@ -6,7 +6,7 @@ export default class RestaurantsController {
         //if restaurantsPerPage exists in the url as query, convert to interger and set it to 10
         // if theres not a query in url, default is 20 per page 
         const restaurantsPerPage = req.query.restaurantsPerPage ? parseInt(req.query.restaurantsPerPage, 10) : 20
-        //if page is in query set it to 10 pages, if not then 0
+        //if page is in query set it to 10 pages, if not then all restaurants are on the main page
         const page = req.query.page ? parseInt(req.query.page, 10) : 0
 
         //will filter based on what shows in the url query
