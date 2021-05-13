701b19dc5a8a4f0117c3a3c92a7c15c20805f42e
ericcapiz <ericcapiz@gmail.com>
2021-05-13 14:05:14 -0500

    started frontend

---
+++ fe/src/App.js
@@ -0,0 +1,13 @@
+import {Switch, Route, Link} from 'react-router-dom'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+function App() {
+  return (
+    <div className="App">
+     App
+    </div>
+  );
+}
+
+export default App;
